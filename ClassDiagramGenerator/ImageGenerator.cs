f3ef46f8f69b7b973708e0902fc1eb91e349cb3b
Andrew Smirthwaite <a.smirthwaite@gmail.com>
2019-11-17 20:02:19 +0000

    Added Attributes, anti aliased text and moved MultiList to it's own class

--- ClassDiagramGenerator/ImageGenerator.cs
+++ ClassDiagramGenerator/ImageGenerator.cs
@@ -4,8 +4,10 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,27 +15,10 @@ namespace ClassDiagramGenerator
 {
     class ImageGenerator
     {
-        private struct MultiList
-        {
-            public string Name;
-            public bool IsStatic;
-            public MultiList(string name, bool isStatic)
-            {
-                Name = name;
-                IsStatic = isStatic;
-            }
-        }
+        
+
         static FontStyle fontStyle = FontStyle.Regular;
-        //Graphics flagGraphics = Graphics.FromImage(image);
-        //int red = 0;
-        //int white = 11;
-        //while (white <= 100)
-        //{
-        //    flagGraphics.FillRectangle(Brushes.Red, 0, red, 200, 10);
-        //    flagGraphics.FillRectangle(Brushes.White, 0, white, 200, 10);
-        //    red += 20;
-        //    white += 20;
-        //}
+
         public static Bitmap GenerateImage(string fileInput, string fileOutput)
         {
 
@@ -48,6 +33,7 @@ namespace ClassDiagramGenerator
             var recHeight = 100 * methods.Capacity;
             var pen = new Pen(Color.Black);
             var font = new Font(FontFamily.GenericSansSerif, 15, fontStyle);
+            imageContent.TextRenderingHint = TextRenderingHint.AntiAlias;
             var textSize = TextRenderer.MeasureText(className, font);
             //overall rectangle
             imageContent.DrawRectangle(pen, 10, 10, 500, recHeight);
@@ -58,7 +44,22 @@ namespace ClassDiagramGenerator
                 10 + textSize.Height/2);
             //seperator line
             imageContent.DrawLine(pen, 10, 10 + textSize.Height*2, 510, 10 + textSize.Height * 2);
-            var methodNameLoc = 10 + textSize.Height * 2 + 10;
+
+            var attributes = FindAttributes(tree);
+
+            var drawLoc = 10 + textSize.Height * 2 + 10;
+            foreach (var attrib in attributes)
+            {
+                font = new Font(FontFamily.GenericSansSerif, 12, (attrib.IsStatic ? FontStyle.Underline : FontStyle.Regular));
+                textSize = TextRenderer.MeasureText(attrib.Name, font);
+                imageContent.DrawString(attrib.Name, font,
+                    new SolidBrush(Color.Black),
+                    15,
+                    drawLoc);
+                drawLoc += textSize.Height + 5;
+            }
+            //seperator line
+            imageContent.DrawLine(pen, 10, drawLoc, 510, drawLoc);
             foreach (var method in methods)
             {
                 font = new Font(FontFamily.GenericSansSerif, 12, (method.IsStatic ? FontStyle.Underline : FontStyle.Regular));
@@ -66,13 +67,8 @@ namespace ClassDiagramGenerator
                 imageContent.DrawString(method.Name, font,
                     new SolidBrush(Color.Black),
                     15,
-                    methodNameLoc);
-                methodNameLoc += textSize.Height + 5;
-            }
-            var attributes = FindAttributes(tree);
-            foreach (var item in attributes)
-            {
-                MessageBox.Show(item.Name);
+                    drawLoc);
+                drawLoc += textSize.Height + 5;
             }
             
 
@@ -110,16 +106,16 @@ namespace ClassDiagramGenerator
                 {
                     modifiers = "+";
                 }
-                else if (modifiers.Contains("private"))
-                {
-                    modifiers = "-";
-                }
                 else if (modifiers.Contains("protected"))
                 {
                     modifiers = "#";
                 }
+                else
+                {
+                    modifiers = "-";
+                }
                 //node.modifiers for accessibilty modifiers
-                attributes.Add(new MultiList(modifiers + node.Declaration, isStatic));
+                attributes.Add(new MultiList(modifiers + OrderAttributes(node.Declaration.ToString()), isStatic));
 
             }
 
@@ -155,7 +151,7 @@ namespace ClassDiagramGenerator
                     modifiers = "#";
                 }
                 //node.modifiers for accessibilty modifiers
-                methods.Add(new MultiList(modifiers + node.Identifier.ToString() + node.ParameterList,isStatic));
+                methods.Add(new MultiList(modifiers + node.Identifier.ToString() + OrderParameters(node.ParameterList.ToString()),isStatic));
                 
             }
 
@@ -164,5 +160,63 @@ namespace ClassDiagramGenerator
         }
 
 
+
+
+
+
+
+        private static string OrderParameters(string parameterList)
+        {
+            string pattern = @"(?:([a-zA-Z0-9_]*) ([a-zA-Z0-9_]*))";
+
+            parameterList = parameterList.Replace(", ",",");
+            parameterList = parameterList.Replace(" ,", ",");
+
+            parameterList = parameterList.Replace("( ", "(");
+            parameterList = parameterList.Replace(" )", ")");
+
+
+
+            var matches = Regex.Matches(parameterList, pattern, RegexOptions.IgnoreCase);
+
+            var sb = new StringBuilder();
+
+            sb.Append("(");
+            foreach (Match match in matches)
+            {
+                sb.Append(match.Groups[2] + ":" + match.Groups[1]);
+                if(match != matches[matches.Count-1])
+                {
+                    sb.Append(",");
+                }
+            }
+            sb.Append(")");
+
+            return sb.ToString();
+        }
+
+        private static string OrderAttributes(string attributes)
+        {
+            string pattern = @"(?:([a-zA-Z0-9_]*) ([a-zA-Z0-9_]*)([=a-zA-Z0-9_. ()]*))";
+
+            attributes = attributes.Replace(", ", ",");
+            attributes = attributes.Replace(" ,", ",");
+
+            attributes = attributes.Replace("( ", "(");
+            attributes = attributes.Replace(" )", ")");
+
+
+
+            var matches = Regex.Matches(attributes, pattern, RegexOptions.IgnoreCase);
+
+            var sb = new StringBuilder();
+
+            foreach (Match match in matches)
+            {
+                sb.Append(match.Groups[2] + ":" + match.Groups[1] + match.Groups[3]);
+            }
+
+            return sb.ToString();
+        }
     }
 }
---
+++ ClassDiagramGenerator/MultiList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDiagramGenerator
+{
+    class MultiList
+    {
+            public string Name;
+            public bool IsStatic;
+            public MultiList(string name, bool isStatic)
+            {
+                Name = name;
+                IsStatic = isStatic;
+            }
+        
+    }
+}
